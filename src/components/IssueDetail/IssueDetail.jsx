45e65119105db2c4aa04bf27ec423ad41bc88be0
hayoenn2 <dwd1205@naver.com>
2023-07-14 02:00:15 +0900

    style | detail style 적용

--- src/components/IssueDetail/IssueDetail.jsx
+++ src/components/IssueDetail/IssueDetail.jsx
@@ -1,4 +1,6 @@
+import moment from 'moment';
 import remarkGfm from 'remark-gfm';
+import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
 import { useParams } from 'react-router-dom';
 import { useContext, useEffect } from 'react';
@@ -13,18 +15,90 @@ export function IssueDetail() {
   }, []);
 
   return (
-    <div>
-      <h3>Issue #{issueItem?.number}</h3>
-      <p>title : {issueItem?.title}</p>
-      <p>작성자: {issueItem?.user.login}</p>
-      <p>작성일: {issueItem?.create_at}</p>
-      <p>코멘트 수: {issueItem?.comments}</p>
-      <p>
-        <img src={issueItem?.user.avatar_url} alt="작성자 프로필 이미지" />
-      </p>
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>
-        {issueItem?.body}
-      </ReactMarkdown>
-    </div>
+    <DetailWrapper>
+      <Div>
+        <Avatar src={issueItem?.user.avatar_url} alt="작성자 프로필 이미지" />
+        <Container>
+          <TitleBox>
+            <DetailNumber>Issue #{issueItem?.number}</DetailNumber>
+            <DetailTitle>{issueItem?.title}</DetailTitle>
+          </TitleBox>
+          <DetailUser>작성자: {issueItem?.user.login}</DetailUser>
+          <DetailDate>
+            작성일: {moment(issueItem?.created_at).format('YYYY-MM-DD')}
+          </DetailDate>
+          <DetailCommnets>코멘트 수: {issueItem?.comments}</DetailCommnets>
+        </Container>
+      </Div>
+      <DetailBody remarkPlugins={[remarkGfm]}>{issueItem?.body}</DetailBody>
+    </DetailWrapper>
   );
 }
+
+const DetailWrapper = styled.div`
+  width: 1000px;
+  margin: 50px auto;
+`;
+
+const Div = styled.div`
+  /* background-color: purple; */
+  display: flex;
+  flex-flow: row;
+  position: relative;
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 10px;
+`;
+
+const Container = styled.div`
+  /* outline: 2px solid red; */
+  display: flex;
+  flex-flow: row wrap;
+  width: 100%;
+  font-weight: 600;
+  margin-left: 10px;
+`;
+
+const TitleBox = styled.div`
+  /* background-color: pink; */
+  width: 100%;
+  display: flex;
+  flex-flow: row nowrap;
+  margin-bottom: 10px;
+`;
+
+const Avatar = styled.img`
+  display: block;
+  width: 80px;
+  height: 80px;
+`;
+
+const DetailNumber = styled.p`
+  /* background-color: skyblue; */
+  width: 120px;
+  font-size: 16px;
+  margin-top: 4px;
+`;
+
+const DetailTitle = styled.h3`
+  /* background-color: lime; */
+  font-size: 24px;
+  margin-left: 10px;
+`;
+
+const DetailUser = styled.p`
+  /* background-color: gold; */
+  margin-right: 15px;
+`;
+
+const DetailDate = styled.p`
+  /* background-color: pink; */
+`;
+
+const DetailCommnets = styled.p`
+  /* background-color: gray; */
+  margin-left: 15px;
+`;
+
+const DetailBody = styled(ReactMarkdown)`
+  padding: 30px 0;
+`;
